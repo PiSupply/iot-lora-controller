6ac4b799dddcac4624c61dd65b9c60c68f5966ff
Ryan Walmsley <ryanteck@gmail.com>
2019-01-07 22:35:35 +0000

    Hopeful final fix

--- viewLogData.php
+++ viewLogData.php
@@ -1,4 +1,7 @@
 <?php
 $logData = shell_exec('journalctl -u iot-lora-gateway.service --no-pager -n 3000');
-echo "<pre>$logData</pre>";
+//$logData = shell_exec('systemctl -l status iot-lora-gateway.service --no-pager');
+echo "<pre>";
+echo $logData;
+echo "</pre>";
  ?>
