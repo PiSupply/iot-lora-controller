69be0a39c3129901cadb71726619d3e2694d98c0
Ryan Walmsley <ryanteck@gmail.com>
2019-11-07 14:21:49 +0000

    Stray .

--- firstTimeSetupP.php
+++ firstTimeSetupP.php
@@ -60,9 +60,9 @@ else {
 
 //Create a Salt
 
-$salt = "7493669DBF".random_int(100000,999999)."135C06E33A45EA".random_int(100000,999999)."9FCFE5"..random_int(100000,999999);
+$salt = "7493669DBF".random_int(100000,999999)."135C06E33A45EA".random_int(100000,999999)."9FCFE5".random_int(100000,999999);
 $configurationFile['user']['salt'] = $salt;
-$salt = "7493669DBF".random_int(100000,999999)."135C06E33A45EA".random_int(100000,999999)."9FCFE5"..random_int(100000,999999);
+$salt = "7493669DBF".random_int(100000,999999)."135C06E33A45EA".random_int(100000,999999)."9FCFE5".random_int(100000,999999);
 
 
 $gatewayID = $_POST['gatewayId'];
